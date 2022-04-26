761c86411348d2d6459b3ea61579b6318fd9333e
Don Brown <mrdon@twdata.org>
2022-04-26 17:40:23 -0600

    Works!

---
+++ deployment-frequency.py
@@ -0,0 +1,82 @@
+import os
+from collections import defaultdict
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from typing import List, Generator
+
+import requests
+from manim import Scene, RED, GREEN, BLUE, YELLOW, ORANGE, BarChart, DrawBorderThenFill, PURPLE, GRAY
+
+CIRCLECI_TOKEN = os.getenv("CIRCLECI_TOKEN", "setme")
+
+
+@dataclass
+class Deploy:
+    on: datetime
+    revision: str
+    url: str
+
+
+def get_deploys_from_jobs(org_slug: str, project_slug: str, pipeline_name: str, job_name: str, branch: str) -> Generator[Deploy, None, None]:
+    start_date = (datetime.utcnow() - timedelta(days=7)).isoformat()
+    resp = requests.get(f"https://circleci.com/api/v2/insights/{org_slug}/{project_slug}/workflows/{pipeline_name}?branch={branch}&start-date={start_date}Z",
+                        auth=(CIRCLECI_TOKEN, ""))
+
+    for item in resp.json()["items"]:
+        workflow_id = item["id"]
+        resp = requests.get(f"https://circleci.com/api/v2/workflow/{workflow_id}/job", auth=(CIRCLECI_TOKEN, ""))
+        for workflow_item in resp.json()["items"]:
+            name = workflow_item["name"]
+            status = workflow_item["status"]
+            if name == job_name and status == "success":
+
+                # get revision
+                resp = requests.get(f"https://circleci.com/api/v2/workflow/{workflow_id}", auth=(CIRCLECI_TOKEN, ""))
+                pipeline_id = resp.json()["pipeline_id"]
+                resp = requests.get(f"https://circleci.com/api/v2/pipeline/{pipeline_id}", auth=(CIRCLECI_TOKEN, ""))
+                revision = resp.json()["vcs"]["revision"]
+
+                on = datetime.fromisoformat(workflow_item["stopped_at"].replace("Z", "+00:00"))
+                yield Deploy(
+                    on=on,
+                    revision=revision,
+                    url=f"https://app.circleci.com/pipelines/{org_slug}/{project_slug}/{pipeline_id}/workflows/{workflow_id}/jobs/MISSING"
+                )
+
+
+def get_deploys_by_day():
+    deploys = defaultdict(list)
+    for deploy in get_deploys_from_jobs(
+            org_slug="gh/sleuth-io",
+            project_slug="sleuth",
+            pipeline_name="test-and-deploy",
+            job_name="deploy-prod",
+            branch="master"):
+        day = deploy.on.date().isoformat()[5:]
+        deploys[day].append(deploy)
+
+    return deploys
+
+
+class DeployFrequencyChart(Scene):
+
+    def construct(self):
+        deploys = get_deploys_by_day()
+        colors = [RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, GRAY]
+        values = [len(values) for values in deploys.values()]
+        names = [key for key in deploys.keys()]
+        bar_chart = BarChart(values, bar_colors=colors, bar_names=names)
+        self.play(DrawBorderThenFill(bar_chart, run_time=5))
+        self.wait(3)
+
+#
+#
+# print(f"deploys per day")
+# for day, deploy_list in deploys.items():
+#     print(f"Day: {day}")
+#     for deploy in deploy_list:
+#         print(f"- {deploy.revision} ({deploy.on})")
+#
+#
+#
+
